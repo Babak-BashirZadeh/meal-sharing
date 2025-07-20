33ee62ac89119553d269e11b9253a05c4356439b
Babak <babak.bashirzadeh@gmaile.com>
2025-07-20 10:53:58 +0200

    update fetch url

--- app-next/app/meals/[id]/page.js
+++ app-next/app/meals/[id]/page.js
@@ -1,7 +1,7 @@
 import MealDetail from "../../../components/MealDetail/MealDetail";
 
 export async function generateStaticParams() {
-  const res = await fetch("http://localhost:3001/api/meals");
+  const res = await fetch("https://meal-sharing-119o.onrender.com/api/meals");
   if (!res.ok) throw new Error(`API failed with ${res.status}`);
   const meals = await res.json();
   return meals.map((meal) => ({ id: meal.id.toString() }));
