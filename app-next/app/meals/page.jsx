df77d1002bfdf47fd2388cea9bd0129dcebdda12
Babak <babak.bashirzadeh@gmaile.com>
2025-07-27 13:40:22 +0200

    update path to fix deploy error

--- app-next/app/meals/page.jsx
+++ app-next/app/meals/page.jsx
@@ -5,7 +5,7 @@ import MealsList from "../../components/MealsList/MealsList";
 
 // Dynamically import MealsList with ssr: false
 // This tells Next.js NOT to render this component on the server during build/prerendering.
-const DynamicMealsList = dynamic(() => import('./MealsList'), {
+const DynamicMealsList = dynamic(() => import('../../components/MealsList/MealsList'), {
   ssr: false, // This is the key!
   loading: () => <p>Loading Meals...</p>, // Optional: A loading component while it loads on client
 });
