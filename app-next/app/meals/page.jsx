489963ccabf034710b8756de5beddc9a95fa2b61
Babak <babak.bashirzadeh@gmaile.com>
2025-07-27 13:29:08 +0200

    add dynamic import ssr to fix errors

--- app-next/app/meals/page.jsx
+++ app-next/app/meals/page.jsx
@@ -3,6 +3,14 @@ export const dynamic = 'force-dynamic';
 import { Suspense } from 'react';
 import MealsList from "../../components/MealsList/MealsList";
 
+// Dynamically import MealsList with ssr: false
+// This tells Next.js NOT to render this component on the server during build/prerendering.
+const DynamicMealsList = dynamic(() => import('./MealsList'), {
+  ssr: false, // This is the key!
+  loading: () => <p>Loading Meals...</p>, // Optional: A loading component while it loads on client
+});
+
+
 export default function MealsPage() {
   return (
     <main>
