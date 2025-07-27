e1b280313fe7b6065b82165bc46f652b07e01833
Babak <babak.bashirzadeh@gmaile.com>
2025-07-27 14:25:54 +0200

    fix local errors return changes

--- app-next/app/meals/page.jsx
+++ app-next/app/meals/page.jsx
@@ -1,20 +1,12 @@
 "use client";
 export const dynamic = 'force-dynamic';
 import { Suspense } from 'react';
-//import MealsList from "../../components/MealsList/MealsList";
-
-// Dynamically import MealsList with ssr: false
-// This tells Next.js NOT to render this component on the server during build/prerendering.
-const DynamicMealsList = dynamic(() => import('../../components/MealsList/MealsList'), {
-  ssr: false, // This is the key!
-  loading: () => <p>Loading Meals...</p>, // Optional: A loading component while it loads on client
-});
-
+import MealsList from "../../components/MealsList/MealsList";
 
 export default function MealsPage() {
   return (
     <main>
-      <DynamicMealsList />
+      <MealsList />
     </main>
   );
 }
