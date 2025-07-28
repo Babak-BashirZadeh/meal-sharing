4af757f65d68fee5d4dd8452306d3b61481584a9
Babak-BashirZadeh <babak.bashirzadeh@gmaile.com>
2025-07-28 17:18:58 +0200

    put mealslist inside suspense tag

--- app-next/app/meals/page.jsx
+++ app-next/app/meals/page.jsx
@@ -1,10 +1,13 @@
 "use client";
+import { Suspense } from "react";
 import MealsList from "../../components/MealsList/MealsList";
 
 export default function MealsPage() {
   return (
     <main>
+      <Suspense fallback={<div>Loading meals...</div>}>
       <MealsList />
+      </Suspense>
     </main>
   );
 }
