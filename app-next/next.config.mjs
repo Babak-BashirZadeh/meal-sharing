37512bd23d255a1430978106f53d69a85503b94f
Babak-BashirZadeh <babak.bashirzadeh@gmaile.com>
2025-07-28 15:03:54 +0200

    update destiniation to api url

--- app-next/next.config.mjs
+++ app-next/next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://meal-sharing-119o.onrender.com/api/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
