17a884fcadacc06becefbda761bbb21f0ad71e5b
Babak-BashirZadeh <babak.bashirzadeh@gmaile.com>
2025-07-28 16:25:47 +0200

    remove suspense and dynamic

--- app-next/app/meals/page.jsx
+++ app-next/app/meals/page.jsx
@@ -1,6 +1,4 @@
 "use client";
-export const dynamic = 'force-dynamic';
-import { Suspense } from 'react';
 import MealsList from "../../components/MealsList/MealsList";
 
 export default function MealsPage() {
