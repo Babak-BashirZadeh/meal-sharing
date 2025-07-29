a993013e06c1e183954f7cca2d7f404f81ec1b8b
Babak-BashirZadeh <babak.bashirzadeh@gmaile.com>
2025-07-29 13:04:10 +0200

    update origin address

--- api/src/index.js
+++ api/src/index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://meal-sharing-119o.onrender.com",
     methods: ["GET", "POST"],
     credentials: true,
   })
